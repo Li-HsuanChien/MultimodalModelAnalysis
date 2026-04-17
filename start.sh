6fbfe9ea1ae51666ed19e2fc74bd9b3ee228ed1e
Li-HsuanChien <shanechien2023@gmail.com>
2026-04-16 20:54:28 -0400

    refactor anaconda scripts

--- start.sh
+++ start.sh
@@ -1,19 +1,2 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-VENV_DIR=".venv"
-
-
-
-pip install --user --upgrade pip --quiet
-
-echo "==> Installing dependencies …"
-pip install --user \
-    torch \
-    transformers \
-    deepface \
-    tf-keras \
-    speechbrain \
-    opensmile \
-    sentencepiece \
-    tqdm
+module load anaconda
+conda activate /storage/home/lpc5553/work/MultimodalModelAnalysis/.conda
